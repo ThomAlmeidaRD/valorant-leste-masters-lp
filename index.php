c431d08f85de292902910edf96314fd9b15da3f0
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2022-04-14 21:13:22 -0300

    estrutura 01

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="container-content">
+
+            <div class="brand-title">
+                <h3>O maior campeonato low elo do Brasil</h3>
+                <h1>Valorant Leste Masters II</h1>
+            </div>
+
+            <div class="users">
+
+                <section class="partners">
+
+                    <ul>
+                        <li><img id="color-1" class="rounded-logos" src="assets/img/logo-team-1" alt=""></li>
+                        <li><img id="color-2" class="rounded-logos" src="assets/img/logo-team-2" alt=""></li>
+                        <li><img id="color-3" class="rounded-logos" src="assets/img/logo-team-3" alt=""></li>
+                        <li><img id="color-4" class="rounded-logos" src="assets/img/logo-team-4" alt=""></li>
+                        <li><b>E mais</b></li>
+                    </ul>
+
+                </section>
+
+                <section class="partners-info">
+                    <b>Equipes Confirmadas</b>
+                    <p>Junte-se a elas na maior competição low-elo BR</p>
+                </section>
+
+                <section>
+                    <button>Realizar inscrição</button>
+                </section>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
