ec674c207ada4adcaf495bce74e4b4b208583e6d
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2022-04-27 08:46:32 -0300

    navbar e branding

---
+++ inc/branding.php
@@ -0,0 +1,24 @@
+<div class="branding">
+
+    <div>
+
+        <video autoplay muted loop>
+            <source src="assets/img/clip.mp4">
+        </video>
+
+        <div class="branding-content">
+
+            <div class="branding-img">
+                <img src="assets/img/vlm.png" alt="">
+            </div>
+
+            <div class="branding-info">
+                <h1>VALORANT LESTE MASTERS II</h1>
+                <p>Evolua para o proximo nivel nos esports participando do maior campeonato low-elo 100% online,</p>
+                <button class="branding-cta">Participar</button>
+            </div>
+
+        </div>
+    </div>
+
+</div>
---
+++ inc/navbar.php
@@ -0,0 +1,3 @@
+<div class="navbar">
+    <b>Precisando de uma equipe pra jogar?, cola no nosso</b> <a href="https://www.discord.com" target="_blank">Discord</a>
+</div>
